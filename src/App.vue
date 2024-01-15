cb79c106e14b0c28376ba4d2791bdca52d095135
sidebar <juantroaquero@gmail.com>
2024-01-15 11:49:34 -0500

    version1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <Tarea/>
+</template>
+
+<script>
+import Tarea from './components/Tarea.vue'
+
+export default {
+  name: "App",
+  components: {
+    Tarea
+  }
+}
+</Script>
+
+<style scoped>
+</style>
